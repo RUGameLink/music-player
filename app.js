4ce4a11faa6f285ce11a83d044b2cfd9f273d1fa
Sad_Smiley <danilvovikov@gmail.com>
2022-10-11 20:11:53 +0800

    Начало дурдома с плеером

--- app.js
+++ app.js
@@ -7,5 +7,28 @@ const audio = document.querySelector('#audio');
 const audioTitle = document.querySelector('.music__title');
 const audioImage = document.querySelector('.music__image');
 
+function loadSongs(song){
+    audioTitle.innerText = song.title;
+    audio.src = `${song.audio}`;
+    audioImage.style.backgroundImage = `url('${song.cover}')`;
+}
+
 let songs;
 const songIndex = 0;
+
+async function retrieveSongFromServer(){
+    await fetch('audio.json')
+        .then((response) => {
+            if(!response.ok){
+                throw new Error('Ошибка загрузки списка треков');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            songs = data.songs;
+            loadSongs(songs[songIndex]);
+        })
+        .catch((error) => {
+            console.error('Не получилось извлечь файл треков:', error);
+        });
+}
