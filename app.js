f45d10006f3df987846b8f29e10695367e3d1fd9
Sad_Smiley <danilvovikov@gmail.com>
2022-10-11 18:54:59 +0800

    Загрузка начинки

---
+++ app.js
@@ -0,0 +1,11 @@
+const musicPlayer = document.querySelector('.musik__player');
+const playBtn = document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const audio = document.querySelector('#audio');
+
+const audioTitle = document.querySelector('.music__title');
+const audioImage = document.querySelector('.music__image');
+
+let songs;
+const songIndex = 0;
